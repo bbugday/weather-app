cbec5403e3272e8837b8a3614360394a85deffee
berke <bugdayberke@gmail.com>
2020-08-29 04:11:00 +0300

    added css

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -7,10 +7,18 @@ const WeatherDetails = () => {
   const {weather,render} = useContext(WeatherContext);
   return render ? (
     <div className="weather-container">
-      <WiCelsius size={100}/>
-      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather main"/>
-      <p className="red">{weather.name}</p>
-      <p>{weather.main.temp}</p>
+      <div className="weather-main">
+        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather main" width="150px" height="150px"/>
+        <div className="temp-and-description">
+          <p className="temp">{(weather.main.temp-273.15).toString().slice(0,5)}°</p>
+          <p className="description">{weather.weather[0].description}</p>
+        </div>
+        
+      </div>
+      <div className="weather-details">
+        
+        <p>{weather.name}</p>        
+      </div>
     </div>
   ) : null;
 } 
