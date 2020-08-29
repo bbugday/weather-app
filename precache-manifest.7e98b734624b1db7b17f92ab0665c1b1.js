e3d011e101324c756410bf461b974afdaad2330f
berke <bugdayberke@gmail.com>
2020-08-29 19:15:14 +0300

    Updates

---
+++ precache-manifest.7e98b734624b1db7b17f92ab0665c1b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f30ff80dd031924ebc10985dae3de612",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e0a19a2fee025a27a335",
+    "url": "/weather-app/static/css/main.2e86b126.chunk.css"
+  },
+  {
+    "revision": "cfe4ee8c34e7827c9832",
+    "url": "/weather-app/static/js/2.98ca3ed1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.98ca3ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a19a2fee025a27a335",
+    "url": "/weather-app/static/js/main.94e47fd1.chunk.js"
+  },
+  {
+    "revision": "bdcb51b63d9249268df7",
+    "url": "/weather-app/static/js/runtime-main.f1c906e7.js"
+  }
+]);
