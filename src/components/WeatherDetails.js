7c5b05cc8368f989399f95f3016359d1c2b802f1
berke <bugdayberke@gmail.com>
2020-08-29 03:05:02 +0300

    css will be added

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,11 +1,15 @@
 import React, { useContext } from "react";
 import {WeatherContext} from '../contexts/WeatherContext';
+import { WiCelsius } from "react-icons/wi";
+
 
 const WeatherDetails = () => {
   const {weather,render} = useContext(WeatherContext);
   return render ? (
-    <div>
-      <p>{weather.name}</p>
+    <div className="weather-container">
+      <WiCelsius size={100}/>
+      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather main"/>
+      <p className="red">{weather.name}</p>
       <p>{weather.main.temp}</p>
     </div>
   ) : null;
